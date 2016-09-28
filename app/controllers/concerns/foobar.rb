3668e7c21ad063e46c5cdfaa3ed7455ea96997b6
christopherquan <chquan@berkeley.edu>
2016-09-28 09:14:30 -0700

    finished hw2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,10 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
   def initialize(param)
+  	@param = param
+  end
+
+  def bar(one, two)
+  	"#{one}#{@param}#{two[:sat]}"
   end
 end
--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,25 +4,40 @@ class Person
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname = @name[0,4]
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'nickname'
+    "#{@nickname}"
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    year = 2016 - @age.to_i
+    "#{year}"
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'introduce'
+    "My name is #{@name} and I am #{@age} years old"
   end
 
   def fib_number
     # YOUR IMPLMENTATION HERE
-    0
+    first = 0
+    second = 1
+    if @age.to_i == 1
+      return first
+    elsif @age.to_i == 2
+      return second
+    else
+      for i in 3..@age.to_i
+        temp = first + second
+        first = second
+        second = temp
+      end
+      return second
+    end
   end
 end
